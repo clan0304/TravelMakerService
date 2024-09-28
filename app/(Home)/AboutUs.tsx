6e1607350b824c3b50e34fef0125c17c953dab15
clan0304 <clan0304@gmail.com>
2024-09-29 09:59:25 +1000

    changed

--- app/(Home)/AboutUs.tsx
+++ app/(Home)/AboutUs.tsx
@@ -1,4 +1,5 @@
 import Grid from './Grid';
+import { motion } from 'framer-motion';
 
 const AboutUs = () => {
   return (
@@ -6,7 +7,14 @@ const AboutUs = () => {
       <h1 className="text-5xl font-bold text-center text-indigo-300/70 font-ubuntu">
         Get to know our services
       </h1>
-      <Grid />
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5, delay: 0, ease: 'easeInOut' }}
+        viewport={{ once: true }}
+      >
+        <Grid />
+      </motion.div>
     </section>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,6 @@ export default function Home() {
   const [googleLists, setGoogleLists] = useState<any[]>([]);
   const serviceSectionRef = useRef<HTMLElement>(null);
 
-  
-
   console.log(googleLists);
   useEffect(() => {
     getGoogleLists('Cafe in Melbourne');
